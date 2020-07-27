fd131503055eebd1ae17201215cdffeb60c5deb8
Adin Hudson <adinhudson1996@gmail.com>
2020-07-27 13:46:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,398 @@
+const videoData = [
+  {
+    id: '25',
+    type: 'தொழில்நுட்பம்',
+    title: 'WHY UBUNTU IS BETTER THAN WINDOWS -TAMIL |தமிழ்',
+    duration: '10:29',
+    url: 'snapchatPost',
+    img: 'ubuntu.jpg',
+    active: 'active',
+    active1: 'show active',
+    trending: true,
+    featured: true
+  },
+  {
+    id: '24',
+    type: 'தொழில்நுட்பம்',
+    title: 'HOW TO INSTALL SSD IN A LAPTOP- TAMIL',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'ssdInstallation.jpg',
+    active: '',
+    active1: '',
+    trending: true,
+    featured: true
+  },
+  {
+    id: '23',
+    type: 'தொழில்நுட்பம்',
+    title: 'Motherboard explained in TAMIL- chipsets, sockets and ports',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'motherBoard2.jpg',
+    active: '',
+    active1: '',
+    trending: true,
+    featured: false
+  },
+  {
+    id: '22',
+    type: 'தொழில்நுட்பம்',
+    title: '90s Kids Kadhal - A TECH view',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: '90stech.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: true
+  },
+  {
+    id: '21',
+    type: 'தொழில்நுட்பம்',
+    title: 'Switching on Ipod Touch after 10 years- Tamil RETRO REVIEW',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: '1stGenipod.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '20',
+    type: 'தொழில்நுட்பம்',
+    title: 'Nvidia VS AMD | எது சிறந்தது?',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'graphicsCard.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '19',
+    type: 'தொழில்நுட்பம்',
+    title: 'DONT BUY AIRPODS PRO BEFORE WATCHING THIS -TAMIL |தமிழ்',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'airpods2ndgen.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '18',
+    type: 'தொழில்நுட்பம்',
+    title: 'Boot time comparison | SSD vs HDD | PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'HddvsSsd.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '17',
+    type: 'தொழில்நுட்பம்',
+    title: 'INSTALL SSD + HDD IN YOUR OLD LAPTOP| TAMIL | SETUP GUIDE-PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'HddSsd.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '16',
+    type: 'பாரம்பரியம்',
+    title: 'கீழடி |KEEZHADI |A TRIBUTE VIDEO BY PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'snapchat.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '15',
+    type: 'தொழில்நுட்பம்',
+    title: 'AIRPODS - Complete Guide -TAMIL |PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'airpods.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '14',
+    type: 'தொழில்நுட்பம்',
+    title: 'Galaxy Buds Review -TAMIL |PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'galaxyBuds.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '13',
+    type: 'தொழில்நுட்பம்',
+    title: 'SAMSUNG S10+ PLUS TAMIL REVIEW | QUICK REVIEW |PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 's10Plus.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '12',
+    type: 'தொழில்நுட்பம்',
+    title: 'iPhone XR Tamil Review | PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'iphonexr.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '11',
+    type: 'விளையாட்டு',
+    title: 'Play PUBG mobile on PC | TAMIL (தமிழ்)',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'pubg.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '10',
+    type: 'தொழில்நுட்பம்',
+    title: 'Samsung S8 & S8+ TAMIL REVIEW | PETROTECH',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 's8plus.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '9',
+    type: 'தொழில்நுட்பம்',
+    title: 'iPAD vs TAB S2 [TAMIL] |PETROTECH-TAMIL TECH CHANNEL',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'ipadvstabS2.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '8',
+    type: 'தொழில்நுட்பம்',
+    title: 'iPad AIR 2 TAMIL|தமிழ் REVIEW |PETROTECH|',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'ipadAir2.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '7',
+    type: 'தொழில்நுட்பம்',
+    title: 'HOW TO APPLY MOBILE SKIN - TAMIL | தமிழ்  GUIDE |PETROTECH|',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'dbrand.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '6',
+    type: 'தொழில்நுட்பம்',
+    title: 'Samsung S7 Edge Review in Tamil | தமிழ்',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 's7.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '5',
+    type: 'தொழில்நுட்பம்',
+    title: 'Bill Gates ஐ பற்றி நீங்கள் அறியாத 17 விடயங்கள்',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'billGates.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '4',
+    type: 'தொழில்நுட்பம்',
+    title: 'தமிழ் | Tamil | HOW TO USE UBER SRILANKA - FREE RIDE CODE INSIDE',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'uberTamil.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '3',
+    type: 'தொழில்நுட்பம்',
+    title: 'LG K10 REVIEW IN TAMIL |தமிழ்',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'lgK10.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '2',
+    type: 'தொழில்நுட்பம்',
+    title: 'iPhone 7 Plus - Full தமிழ் Review (மதிப்பாய்வுரை)',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'iphone7Plus.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  },
+  {
+    id: '1',
+    type: 'தொழில்நுட்பம்',
+    title: 'ஸ்நப் சாட் என்றால் என்ன ?',
+    duration: '21:29',
+    url: 'snapchatPost',
+    img: 'snapchat.jpg',
+    active: '',
+    active1: '',
+    trending: false,
+    featured: false
+  }
+];
+
+function videotemplate1 (video) {
+  return `
+  <div class="tab-pane fade ${video.active1}" id="post-${video.id}" role="tabpanel" aria-labelledby="post-${video.id}-tab">
+                            
+    <div class="single-feature-post video-post bg-img" style="background-image: url(img/bg-img/${video.img});">
+         
+      <a href="${video.url}.html" class="btn play-btn"><i class="fa fa-play" aria-hidden="true"></i></a>
+          
+      <div class="post-content">
+        <a href="#" class="post-cata" lang="tt">${video.type}</a>
+        <a href="${video.url}.html" class="post-title" lang="tt">${video.title}</a>
+          <div class="post-meta d-flex">
+          </div>
+      </div>
+          
+      <span class="video-duration">${video.duration}</span>
+    </div>
+  </div>
+  `;
+}
+
+function videotemplate2 (video) {
+  return `
+  <a class="nav-link ${video.active}" id="post-${video.id}-tab" data-toggle="pill" href="#post-${video.id}" role="tab" aria-controls="post-${video.id}" aria-selected="true">
+                                
+  <div class="single-blog-post style-2 d-flex align-items-center">
+    <div class="post-thumbnail">
+      <img src="img/bg-img/${video.img}" alt="">
+    </div>
+    <div class="post-content">
+      <h6 class="post-title" lang="tt">${video.title}</h6>
+      <div class="post-meta d-flex justify-content-between">
+      </div>
+    </div>
+  </div>
+  </a>                   
+  `;
+}
+
+function trendingTemplate (video) {
+  if (video.trending === true) {
+    return `
+    <div class="col-12 col-md-4">
+      <div class="single-post-area mb-80">
+        <div class="post-thumbnail">
+            <img src="img/bg-img/${video.img}" alt="">
+            <span class="video-duration">${video.duration}</span>
+        </div>
+        <div class="post-content">
+            <a href="#" class="post-cata">${video.type}</a>
+            <a href="${video.url}.html" class="post-title">${video.title}</a>
+            <div class="post-meta d-flex">
+            </div>
+        </div>
+      </div>
+    </div>`;
+  }
+}
+
+function featuredPost (video) {
+  if (video.featured === true) {
+    return `
+      <div class="single-feature-post video-post bg-img" style="background-image: url(img/bg-img/${video.img});">
+        <a href="${video.url}.html" class="btn play-btn"><i class="fa fa-play" aria-hidden="true"></i></a>
+        <div class="post-content">
+          <a href="#" class="post-cata">${video.type}</a>
+          <a href="${video.url}.html" class="post-title">${video.title}</a>
+          <div class="post-meta d-flex">
+          </div>
+        </div>
+      <span class="video-duration">${video.duration}</span>
+    </div>
+`;
+  }
+}
+
+document.getElementById('app').innerHTML = `
+  ${videoData.map(videotemplate1).join('')}
+`;
+
+document.getElementById('app1').innerHTML = `
+<ul class="nav vizew-nav-tab" role="tablist">
+  <li class="nav-item">
+    ${videoData.map(videotemplate2).join('')}
+  </li>
+</ul>
+`;
+
+document.getElementById('app2').innerHTML = `
+  ${videoData.map(trendingTemplate).join('')}
+`;
+
+document.getElementById('app3').innerHTML = `
+  ${videoData.map(featuredPost).join('')}
+`;
