675088ea63427c47d9b6aa0bd2372f54f27b6914
Adin Hudson <adinhudson1996@gmail.com>
2020-08-06 00:25:43 +0530

    minor changes

--- aboutUs.js
+++ aboutUs.js
@@ -41,7 +41,7 @@ function teamTemplate (data) {
   return `
   <div class="col-md-3 col-sm-6" style="margin-right: 40px;  margin-bottom: 30px;">
   <div class="full team_blog_colum">
-      <img class="img-responsive" style="border-radius: 50%;" src="img/bg-img/${data.img}">
+      <img class="img-responsive" src="img/bg-img/${data.img}">
         <div class="team_feature_head" style="margin-bottom: 20px; margin-top: 10px">
         <h4>${data.name}</h4>
         <h7 style="color: #db4437">${data.designation}</h7>
